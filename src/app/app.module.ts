5dff17ca5f78c73e3f51af7c6913bd99aa6cf780
akashjadhav1221 <softdev1@artiligent.in>
2021-03-04 17:59:09 +0530

    Bootstrap added. Task related components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ListTaskComponent } from './list-task/list-task.component';
+import { TaskComponent } from './list-task/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ListTaskComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
