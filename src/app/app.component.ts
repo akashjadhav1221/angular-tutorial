7dca60287bb5a5e4f6a7a52500f50f8341842aab
akashjadhav1221 <softdev1@artiligent.in>
2021-03-04 20:33:06 +0530

    Data passing between parent child components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorial';
+  task = '';
+
+  add(task: string){
+    console.log('Recieved', task);
+    this.task = task;
+  }
 }
--- src/app/list-task/list-task.component.ts
+++ src/app/list-task/list-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-task',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTaskComponent implements OnInit {
 
+  @Input() task: string;
+  taskList = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(){
+    console.log('OnChanges', this.task);
+    if(this.task.trim().length > 0)
+    this.taskList.push(this.task);
   }
 
 }
