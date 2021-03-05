e9d1014e5586f976b29abed6af1ed56283d1122e
akashjadhav1221 <softdev1@artiligent.in>
2021-03-05 17:39:33 +0530

    Better highlight directive improved.

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { OnInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import { OnInit, Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[betterhighlight]'
@@ -12,5 +12,14 @@ export class BetterHighlight implements OnInit{
 
     ngOnInit(): void {
       this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+    }
+
+    @HostListener('mouseenter') mouseEnter(eventData: Event){
+        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    }
+
+    @HostListener('mouseleave') mouseLeave(eventData: Event){
+        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
     }
 }
